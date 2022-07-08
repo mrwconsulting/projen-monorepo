ed2ee8ac0f1b55238931ec2e9a9d4fe864a10c07
Mark Washburn <mark@mrwconsulting.tech>
2022-07-08 18:04:29 -0500

    chore: projen cleanup

--- .projenrc.js
+++ .projenrc.js
@@ -2,14 +2,10 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Mark Washburn',
   authorAddress: 'mark@mrwconsulting.tech',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.31.0',
   defaultReleaseBranch: 'main',
-  name: 'projen-monorepo',
-  repositoryUrl: 'https://github.com/mrwconsulting/projen-monorepo.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  name: '@mrwconsulting/projen-monorepo',
+  packageName: '@mrwconsulting/projen-monorepo',
+  repositoryUrl: 'https://github.com/mrwconsulting/projen-monorepo.git'
 });
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@mrwconsulting/projen-monorepo.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@mrwconsulting/projen-monorepo.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@mrwconsulting/projen-monorepo'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@mrwconsulting/projen-monorepo.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@mrwconsulting/projen-monorepo.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
