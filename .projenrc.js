1dd9ee6f287b0110e03d6df8f3eb87af40a2376a
Mark Washburn <mark@mrwconsulting.tech>
2022-07-09 12:33:07 -0500

    fix: added beta branch

--- .projenrc.js
+++ .projenrc.js
@@ -5,11 +5,19 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'mark@mrwconsulting.tech',
   cdkVersion: '2.31.0',
   defaultReleaseBranch: 'main',
+  majorVersion: 1,
   name: '@mrwconsulting/projen-monorepo',
   packageName: '@mrwconsulting/projen-monorepo',
   repositoryUrl: 'https://github.com/mrwconsulting/projen-monorepo.git',
   npmAccess: NpmAccess.PUBLIC,
   npmRegistryUrl: 'https://npm.pkg.github.com',
   releaseToNpm: true,
+  releaseBranches: {
+    beta: {
+      majorVersion: 1,
+      prerelease: true,
+      tagPrefix: 'beta',
+    },
+  },
 });
 project.synth();
